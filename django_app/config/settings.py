36ff15706e2874a055af39a12c9dc4d300f265bb
Hyeongjin Kim <hyeongjin184@gmail.com>
2017-08-11 16:10:08 +0900

    makemigrations, migrate 생성완료

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -15,6 +15,9 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Custom User
+AUTH_USER_MODEL = 'member.User'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -37,6 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django_extensions',
+
+    'member',
+    'post',
 ]
 
 MIDDLEWARE = [
--- django_app/post/models.py
+++ django_app/post/models.py
@@ -1,5 +1,59 @@
+from django.conf import settings
 from django.db import models
 
 
 class Post(models.Model):
-    pass
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
+    photo = models.ImageField(blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+    like_users = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name='like_users',
+        through='PostLike',
+    )
+    tags = models.ManyToManyField('Tag')
+
+    def add_comment(self, user, content):
+        return self.comment_set.create(author=user, content=content)
+
+    def add_tag(self, tag_name):
+        tag, tag_created = Tag.objects.get_or_create(name=tag_name)
+        if not self.tags.filter(id=tag.id).exists():
+            self.tags.add(tag)
+
+    @property
+    def like_count(self):
+        return self.like_users.count()
+
+
+class PostLike(models.Model):
+    post = models.ForeignKey(Post)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
+    content = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+    like_users = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name='like_comments',
+        through='CommentLike'
+    )
+
+
+class CommentLike(models.Model):
+    comment = models.ForeignKey(Comment)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return 'Tag({})'.format(self.name)
