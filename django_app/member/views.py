435d3bd65ed35e9cbef5ed9cb40fbf0232b71ae2
Hyeongjin Kim <hyeongjin184@gmail.com>
2017-08-15 15:13:37 +0900

    login view 완성

--- django_app/member/views.py
+++ django_app/member/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login as django_login
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
 
 def login(request):
     if request.method == 'POST':
-        pass
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(
+            request,
+            username=username,
+            password=password,
+        )
+        if user is not None:
+            django_login(request, user)
+            return redirect('post:post_list')
+        else:
+            return HttpResponse('Login credentials invalid')
     else:
+        if request.user.is_authenticated:
+            return redirect('post:post_list')
         return render(request, 'member/login.html')
